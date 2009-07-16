c877abb0c7f3eeefeec61a9a46d2efe520bc58d9
mullejea <mullejea@853d4b04-0d2b-4dc3-919d-a27cf6073fd5>
2009-07-16 14:36:01 +0000

    Mediawiki

--- db/addedTables.sql
+++ db/addedTables.sql
@@ -36,3 +36,5 @@ PRIMARY KEY ( `rev_id` , `session_id` )
 `value` BIGINT( 18 ) NOT NULL DEFAULT '0',
  `server_id` VARCHAR( 40 ) NOT NULL DEFAULT '0'
 ) ENGINE = InnoDB  DEFAULT CHARSET = latin1;
+
+INSERT INTO `p2p_params` (`value`, `server_id`) VALUES ('0', '0');
